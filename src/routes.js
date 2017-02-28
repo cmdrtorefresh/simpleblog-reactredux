faff6646914a165e42b491a91ffe41050987c46e
Stephanie Kirtiadi <skirtiadi@giantrabbit.com>
2017-02-28 00:09:56 -0800

    Routing post id page with index.

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import App from './components/app';
 
 import PostsIndex from './containers/posts_index';
 import PostsNew from './containers/posts_new';
+import PostsShow from './containers/posts_show';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex} />
     <Route path="posts/new" component={PostsNew} />
+    <Route path="posts/:id" component={PostsShow} />
   </Route>
 );
+// this.props.params.id
